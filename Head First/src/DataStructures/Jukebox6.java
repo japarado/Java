315ff5e9ddb71992a0c1b9b3a1a9b7df470e675c
Justin A. Parado <japarado@student.apc.edu.ph>
2017-11-01 10:43:26 +0800

    Ignored .idea folder (corrected path)

---
+++ Head First/src/DataStructures/Jukebox6.java
@@ -0,0 +1,18 @@
+package DataStructures;
+
+import java.util.ArrayList;
+
+public class Jukebox6
+{
+    ArrayList<Song> songList = new ArrayList<Song>();
+
+    public static void main(String[] args)
+    {
+        new Jukebox6().go();
+    }
+
+    public void go()
+    {
+
+    }
+}
